3a3e6c0c9c3d9c8efc154d6e66d741ab0c11aee9
icmp <jmfield2@mail.usf.edu>
2014-02-12 15:27:39 +0000

    missing comma

--- settings.py
+++ settings.py
@@ -467,7 +467,7 @@ WHOOSH_INDEX = path.join(PROJECT_ROOT, 'storage/search')
 
 CACHES = {
        'default': {
-               'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache'
+               'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
                'LOCATION': '127.0.0.1:11211',
        }
 }
